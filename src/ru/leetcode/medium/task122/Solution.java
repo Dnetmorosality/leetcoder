8e7882de0c833ed144679de287abe6e497f480e7
Dnetmorosality <rustam256@mail.ru>
2026-02-13 20:57:52 +0300

    second solution

--- src/ru/leetcode/medium/task122/Solution.java
+++ src/ru/leetcode/medium/task122/Solution.java
@@ -4,22 +4,28 @@ class Solution {
     public int maxProfit(int[] prices) {
         int l = 0;
         int result = 0;
+        int r = prices.length - 1;
         boolean needSell = false;
-        while (l < prices.length - 1) {
-            while (l < prices.length - 1 && prices[l] > prices[l + 1]) l++;
-            if (l == prices.length - 1 && result == 0) return 0;
+        while (l < r) {
+            while (l < r && prices[l] > prices[l + 1]) l++;
+            if (l == r) {
+                if (!needSell) {
+                    return result;
+                } else {
+                    result = result + prices[l];
+                }
+            }
             if (l < prices.length) {
                 result = result - prices[l];
                 needSell = true;
             }
             l++;
-            while (l < prices.length - 1 && prices[l] < prices[l + 1]) l++;
+            while (l < r && prices[l] < prices[l + 1]) l++;
             if (l < prices.length) {
                 result = result + prices[l];
                 needSell = false;
             }
             l++;
-            if (l == prices.length - 1 && needSell) result = result + prices[l];
         }
         return result;
     }
